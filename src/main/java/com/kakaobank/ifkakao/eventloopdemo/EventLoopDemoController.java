584117139c9296f3be30082f837c67bfbb29503e
dy.ho <dy.ho@kakaobank.com>
2021-08-24 11:34:55 +0900

    Set Daemon Scheduler for the Blocking call

--- src/main/java/com/kakaobank/ifkakao/eventloopdemo/EventLoopDemoController.java
+++ src/main/java/com/kakaobank/ifkakao/eventloopdemo/EventLoopDemoController.java
@@ -4,12 +4,14 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
 
 @RestController
 public class EventLoopDemoController {
     @GetMapping(value = "/sleep", produces = MediaType.TEXT_PLAIN_VALUE)
     public Mono<String> sleep() {
-        return Mono.fromSupplier(() -> blockingFunction(10_000L));
+        return Mono.fromSupplier(() -> blockingFunction(10_000L))
+                .subscribeOn(Schedulers.boundedElastic());
     }
 
     private String blockingFunction(long sleepMs) {
